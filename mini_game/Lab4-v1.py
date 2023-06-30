e71c4b104375564d5cebb9866a6b0ebe648cbf09
Matheus Caldeira Brant <mathcbvc@gmail.com>
2023-06-30 16:08:55 -0300

    Update Lab4

--- mini_game/Lab4-v1.py
+++ mini_game/Lab4-v1.py
@@ -74,18 +74,27 @@ class Hangman:
         self.word = random.choice(["abacate","vandalo","ombrelone","cortina","jurassico","ampola","obsidiana","troia","unicornio"])
         self.triesLeft = len(board)
 	# Método para adivinhar a letra
-    def guess(self, x):
-        return x in self.word
+    def guess(self, attempt):
+        if attempt in self.word:
+            #substituir _ na palavra se a palavra tiver a letra 
+            self.triesLeft -= 1
+            pass
+        else:
+            self.triesLeft -= 1
 	# Método para verificar se o jogo terminou
     def isOver(self):
-        pass
+        #testa se na palavra já não possui nenhum '_'
+        if self.triesLeft == 0 : 
+            print("GAME OVER") 
     # Método para verificar se o jogador venceu
     def isWinner(self):
-        pass	
+        if 	'_' not in self.word and self.triesLeft > 0 : 
+            print("VOCE GANHOU!")
 	# Método para não mostrar a letra no board
-    def hideStatus(self):
-         pass
+    def hideWord(self):
+        #imprimir na tela apenas os traços ao invés da palavra. Criar uma lista secundária contendo apenas '_'
+        self.temp = ['_' for x in self.word]
 	# Método para checar o status do game e imprimir o board na tela
-    def showStatus(self):
-         pass
-
+    def boardController(self):
+        #controlar o board do game. 
+        pass
